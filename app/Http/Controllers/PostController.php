1e8dfd8aa3e9f0782b55afdb0744366b7e7e9cde
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-12-10 02:39:07 -0600

    ✅ Adding Tests: succesfully send message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+// use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Api;
 
 use Illuminate\Http\Request;
 use App\Post;
@@ -14,10 +16,29 @@ class PostController extends Controller
 
    public function publish(Post $post){
        $post->update(
-           [
+           [ 
             'is_published' => true
            ]
        );
+       $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
+
+       $message = "OFERTA DE TRABAJO\n!Urgente $post->title!\n\nUbicación de la oferta - $post->location\n$post->description\n\nMás información en $post->url";
+    
+       $response = $telegram->sendMessage([
+        'chat_id' => '@scraping_web_pruebas', 
+        'text' => $message
+      ]);
+   
+     
+       
+
        return back();
 }
+
+    public function sendMessage($notifiable){
+    TelegramFile::create()
+        ->to($notifiable->telegram_user_id) // Optional
+        ->content('Did you know we can set a custom filename too?')
+        ->document('https://file-examples-com.github.io/uploads/2017/10/file-sample_150kB.pdf', 'sample.pdf');
+    }
 }
